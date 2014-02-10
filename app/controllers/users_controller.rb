fa6ce607e06c60039d31171d7a2919fafb1029ce
Tommy Duek <taduek@gmail.com>
2014-02-10 18:01:24 -0500

    Move auth logic into application_controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
     @user = User.new(params[:user])
 
     if @user.save
-      self.current_user = @user
+      sign_in(@user)
       redirect_to user_url(@user)
     else
       render :json => @user.errors.full_messages
